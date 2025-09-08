1b0cfd3a57c89aa06831c5fa7549936274e8423c
hoangtk <concrete@CC-Airs-MacBook-Air.local>
2025-09-08 11:37:59 +0700

    edit home page, 8/9 11:37

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="banner__content">
             <div class="banner__logo banner__logo--left">
-                <img src="{{ asset('images/ISO_9001-2015.svg') }}" alt="ISO 9001:2015" class="banner__logo-image">
+                <img src="{{ asset('images/ISO_9001-2015.png') }}" alt="ISO 9001:2015" class="banner__logo-image">
             </div>
             <h1 class="banner__title">ISO ĐIỆN TỬ NHÀ MÁY A31</h1>
             <div class="banner__logo banner__logo--right">
-                <img src="{{ asset('images/ISO_9001-2015.svg') }}" alt="ISO 9001:2015" class="banner__logo-image">
+                <img src="{{ asset('images/ISO_9001-2015.png') }}" alt="ISO 9001:2015" class="banner__logo-image">
             </div>
         </div>
     </div>
